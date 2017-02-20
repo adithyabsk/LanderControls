cc5ac9f9b4d8005de83033da1445a1c43ded1f3e
Adithya Balaji <adithyabsk@gmail.com>
2017-02-20 08:00:32 -0800

    Working GPS and Camera drivers

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # LanderControls
 Lander Code for the BOTR competition
+
+Required ROS Packages
+OpenCV
+cv_bridge
+nmea_navsat_driver
+
---
+++ lander_ws/src/sensor_nodes/src/image_publisher.cpp
@@ -0,0 +1,35 @@
+#include <ros/ros.h>
+#include <image_transport/image_transport.h>
+#include <opencv2/highgui/highgui.hpp>
+#include <cv_bridge/cv_bridge.h>
+#include <sstream> // for converting the command line parameter to integer
+
+int main(int argc, char** argv)
+{
+  ros::init(argc, argv, "c270ImagePublisher");
+  ros::NodeHandle nh;
+  image_transport::ImageTransport it(nh);
+  image_transport::Publisher pub = it.advertise("/c270/image_raw", 1);
+
+  int video_source = 0;
+
+  cv::VideoCapture cap(video_source);
+  // Check if video device can be opened with the given index
+  if(!cap.isOpened()) return 1;
+  cv::Mat frame;
+  sensor_msgs::ImagePtr msg;
+
+  ros::Rate loop_rate(5);
+  while (nh.ok()) {
+    cap >> frame;
+    // Check if grabbed frame is actually full with some content
+    if(!frame.empty()) {
+      msg = cv_bridge::CvImage(std_msgs::Header(), "bgr8", frame).toImageMsg();
+      pub.publish(msg);
+      cv::waitKey(1);
+    }
+
+    ros::spinOnce();
+    loop_rate.sleep();
+  }
+}
