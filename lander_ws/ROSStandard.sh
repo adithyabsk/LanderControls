ed3c92357a697cae559194079e19a1b90a3d1f27
Alexander Allen <admin@altechcode.com>
2017-02-23 16:23:09 -0500

    Add ROS Setup script

---
+++ lander_ws/ROSStandard.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Setup ROS for Local Development
+source /opt/ros/kinetic/setup.bash
+source ~/Rocketry/LanderControls/lander_ws/devel/setup.bash
+export ROS_MASTER_URI=http://localhost:11311
+export ROS_IP=$(hostname -I)
+export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
+
+exec "$@"
