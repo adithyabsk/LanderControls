45ea821f423b72b9645a9c43595c244f684fe8fa
Adithya Balaji <adithyabsk@gmail.com>
2017-04-18 10:35:06 -0700

    More commits, I have no idea what was changed

---
+++ lander_ws/ROSLander.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Setup ROS for Local Development
+source /opt/ros/kinetic/setup.bash
+source ~/LanderControls/lander_ws/devel/setup.bash
+export ROS_MASTER_URI=http://10.9.0.14:11311
+export ROS_IP=10.9.0.10
+export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
+
+exec "$@"
